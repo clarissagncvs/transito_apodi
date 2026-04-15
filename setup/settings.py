b2555b6575b8a0c48de8759f674d589313e5c125
clarissagncvs <clarissagncvs@gmail.com>
2026-04-15 08:33:35 -0300

    adicionando django rest framework e ativando o CORS

--- setup/settings.py
+++ setup/settings.py
@@ -31,7 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+#CORS (Cross-Origin Resource Sharing) é um mecanismo de segurança baseado em cabeçalhos HTTP que permite que navegadores liberen ou bloqueiem requisições entre origens diferentes (domínios, protocolos ou portas distintas). Na fase de desenvolvimento ele ficará liberado.
+CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'ocorrencias',
     'semaforos',
     'usuarios',
@@ -48,6 +51,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#configurações do django rest framework
+#PERMISSION_CLASSES
+#AllowAny → qualquer um pode acessar a API
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
