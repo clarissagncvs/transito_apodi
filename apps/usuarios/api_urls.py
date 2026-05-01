4146e772c27c3bc35fc8a591724260ebe3d5860f
Natan Lincoln <natanarthss51@gmail.com>
2026-05-01 12:29:11 -0300

    feita a API para o mapa; renomeei o nome dos arquivos views_api para api_views (para ficar mais organizado e padronizado); adicionei um bagulholá no settings para que a atualização do mapa seja armazenada no cache e não no db pra ele não explodir

--- apps/usuarios/api_urls.py
+++ apps/usuarios/api_urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 
-from .views_api import UsuarioViewSet
+from .api_views import UsuarioViewSet
 
 router = DefaultRouter()
 router.register(r"", UsuarioViewSet, basename="usuario")
