600d758e8d4329ee7b795836f5cdedfb253eccdb
clarissagncvs <clarissagncvs@gmail.com>
2026-05-02 12:14:58 -0300

    flake8

--- setup/settings.py
+++ setup/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['transitoapodi-production.up.railway.app', "*", 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['transitoapodi-production.up.railway.app', "*", 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = [
     'https://transitoapodi-production.up.railway.app',
     'https://*.127.0.0.1'
