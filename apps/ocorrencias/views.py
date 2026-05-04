7c8a5d3e2df24d44be8f51b5ef98e28bb5e61109
Ariel <alissonariel.u@gmail.com>
2026-05-04 02:09:14 -0300

    geração de alertas e forms vazios

--- apps/ocorrencias/views.py
+++ apps/ocorrencias/views.py
@@ -1,17 +1,78 @@
 # Importa a função render, usada para retornar templates HTML
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Ocorrencia, Alerta
 
+from .forms import OcorrenciaForm
+from django.contrib.auth.decorators import login_required
 
 # Importa HttpResponse, usado para retornar respostas simples (texto puro, por exemplo)
 # View responsável por exibir a lista de ocorrências
 def lista(request):
+#busca todas as ocorrencias do banco de dados
+    ocorrencias = Ocorrencia.objects.select_related('via', 'usuario').all()
+    
+    tipo_filtro   = request.GET.get('tipo', '')
+    status_filtro = request.GET.get('status', '')
+    busca         = request.GET.get('busca', '')
+   
+    if tipo_filtro:
+        ocorrencias = ocorrencias.filter(tipo=tipo_filtro)
 
-    # Renderiza (carrega) o template HTML localizado em:
-    # templates/ocorrencia/ocorrencias.html
-    # e retorna como resposta para o navegador
+    if status_filtro:
+        ocorrencias = ocorrencias.filter(status=status_filtro)
+
+    if busca:
+        ocorrencias = ocorrencias.filter(
+            Q(descricao__icontains=busca) |
+            Q(via__nome__icontains=busca)
+        )
+
+#dicionario python que envia dados ao HTML
+    contexto = {
+        'ocorrencias': ocorrencias, #lista de ocorrecias filtradas
+        'tipos': Ocorrencia.Tipo.choices,
+        'status.opcoes': Ocorrencia.Status.choices, 
+        'tipo_filtro': status_filtro, #filtro atual
+        'status_filtro': tipo_filtro, 
+        'busca': busca,
+    }
+# Renderiza (carrega) o template HTML localizado em:
+# templates/ocorrencia/ocorrencias.html
+# e retorna como resposta para o navegador
     return render(request, "pages/ocorrencias.html")
 
+@login_required
+def nova(request):
+#Se o usuário enviou o formulário
+    if request.method == 'POST':
+        form = OcorrenciaForm(request.POST, request.FILES)
+        #valida todos os campos
+        #retorna True se tudo estiver ok, False se houver erro
+        if form.is_valid():
+            #cria o objeto Ocorrencia, mas precisa adiionar o campo 'usuario' antes de salvar no bd
+            #commit=False: prepara a ocorrência, mas não registra ainda
+            oc = form.save(commit=False)
+            #request.user: o usuario está logado agora
+            #registra quem fez a ocorrência
+            oc.usuario = request.user
+            #salva no banco de dados
+            oc.save()
+#gera o alerta automático
+        if oc.tipo in ('ACIDENTE', 'SEMAFORO'):
+            nivel = 'CRITICO' if oc.tipo == 'ACIDENTE' else 'AVISO'
 
-# Comentário padrão criado pelo Django ao gerar o arquivo views.py
-# Pode ser removido se quiser, não tem função prática
-# Create your views here.
+            mensagem = f'{oc.get_tipo_display()} em {oc.via}'
+#Cria e salva o alerta no banco
+            Alerta.objects.create(
+                ocorrencia=oc, #liga o alerta à ocorrência
+                nivel=nivel,
+                mensagem=mensagem,
+            )
+#Manda o navegador ir para a lista
+        return redirect('ocorrencias:lista')
+#exibe um formulário em branco pro usuáio preencher
+    else:
+        form = OcorrenciaForm() #formulario vazio
+
+    return render(request, 'pages/ocorrencias.html', {'ocorrencias': form})
