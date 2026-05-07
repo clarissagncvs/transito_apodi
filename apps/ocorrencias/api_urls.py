243ab85aa68d2646a47f112fe3f23f91b65823a5
aianea <aianealmeidaprax@gmail.com>
2026-05-07 14:30:01 -0300

    formatação

--- apps/ocorrencias/api_urls.py
+++ apps/ocorrencias/api_urls.py
@@ -3,10 +3,10 @@ from rest_framework.routers import DefaultRouter
 from .api_views import OcorrenciaViewSet
 
 # Desativamos o trailing_slash ou usamos o SimpleRouter para evitar o registro duplo
-router = DefaultRouter(trailing_slash=False) 
+router = DefaultRouter(trailing_slash=False)
 router.register(r"lista", OcorrenciaViewSet, basename="ocorrencia")
 
 # Pegamos as urls brutas sem passar pelo format_suffix_patterns
 urlpatterns = [
-    path("", include(router.get_urls())), # Usamos get_urls() diretamente
+    path("", include(router.get_urls())),  # Usamos get_urls() diretamente
 ]
