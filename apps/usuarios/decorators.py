a38cf34b21c6120e6c44f897464d0210a81dc1a1
aianea <aianealmeidaprax@gmail.com>
2026-05-07 14:28:02 -0300

    homes para admin e agente

---
+++ apps/usuarios/decorators.py
@@ -0,0 +1,24 @@
+from functools import wraps
+from django.shortcuts import redirect
+
+
+def admin_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        if not request.user.is_admin_transito:
+            return redirect('home')
+        return view_func(request, *args, **kwargs)
+    return wrapper
+
+
+def agente_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        if not request.user.is_agente:
+            return redirect('home')
+        return view_func(request, *args, **kwargs)
+    return wrapper
