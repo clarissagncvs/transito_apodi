3c74a1ff21da9587451c1fcd9e914914abf18350
Rute <anarutemartins13@gmail.com>
2026-05-07 21:59:59 -0300

    modificados

--- apps/ocorrencias/forms.py
+++ apps/ocorrencias/forms.py
@@ -5,7 +5,7 @@ from .models import Ocorrencia
 class OcorrenciaForm(forms.ModelForm):
     class Meta:
         model = Ocorrencia
-        fields = ['tipo', 'via', 'descricao', 'semaforo', 'horario_incidente']
+        fields = ['tipo', 'via', 'descricao', 'semaforo', 'horario_incidente', 'latitude', 'longitude']
 
         widgets = {
             'tipo': forms.Select(attrs={'class': 'input'}),
@@ -17,12 +17,15 @@ class OcorrenciaForm(forms.ModelForm):
             }),
             'semaforo': forms.Select(attrs={'class': 'input'}),
             'horario_incidente': forms.TimeInput(attrs={'class': 'input', 'type': 'time'}),
+            'latitude': forms.HiddenInput(),
+            'longitude': forms.HiddenInput(),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Deixa o campo semáforo opcional no formulário
         self.fields['semaforo'].required = False
+        self.fields['latitude'].required = False
+        self.fields['longitude'].required = False
         self.fields['tipo'].label = "Tipo de ocorrência"
         self.fields['via'].label = "Endereço (Via)"
         self.fields['descricao'].label = "Descrição"
